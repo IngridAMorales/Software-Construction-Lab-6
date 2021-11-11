0d8e4580ad64797e53d8d38016b3c3c203cfaa17
Viviane Fernando <viviane9533@gmail.com>
2021-11-11 12:18:49 -0800

    Finish and tested Select Or Function

---
+++ select_or.hpp
@@ -0,0 +1,34 @@
+#ifndef __SELECT_OR_HPP__
+#define __SELECT_OR_HPP__
+
+#include <cstring>
+#include "select.hpp"
+
+class Select_Or : public Select{
+private:
+
+Select* select1;
+Select* select2;
+public:
+
+     Select_Or(Select* nselect1, Select* nselect2){
+
+	select1 = nselect1;
+	select2 = nselect2;
+
+	}
+     bool select (const Spreadsheet* sheet, int row) const{
+	if (select1->select(sheet,row)||select2->select(sheet,row)){
+		return true;
+}
+	else{
+		return false;
+}	
+}
+};
+
+
+
+
+
+#endif //__SELECT_OR_HPP__
