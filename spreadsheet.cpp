d667dbaf0586bc50e62bcd9abb7c5d6dc6504e5b
IngridAMorales <ingridmorales1016@gmail.com>
2021-11-11 10:18:49 -0800

    Print_selection function under testing

--- spreadsheet.cpp
+++ spreadsheet.cpp
@@ -45,7 +45,11 @@ void Spreadsheet::print_selection(std::ostream& out) const
     for(int i = 0; i<data.size(); i++) {
 	for(int j= 0; j < data[i].size(); j++){
 		if(select== nullptr) { 
-	           out<<data[i][j]<<" "; }
+	           out<<data[i][j]<<" "; 
+		}
+		else if(select->select(data[i][j])){
+		   out<<data[i][j]<<" "; 
+		}	  
     	}
 	out<<"\n"; 
    }
